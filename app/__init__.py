6d0f1cda2f63eabd6e8337d6f5fc4dcf10e8f771
danny <dantonzhao@gmail.com>
2017-10-05 23:28:38 -0400

    switching to oauth instead of openid

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config') #reads config file
+
 from app import views
---
+++ config.py
@@ -0,0 +1,4 @@
+#cross-site request forgery prevention
+WTF_CSRF_ENABLED = True
+
+SECRET_KEY = 'somekey'
